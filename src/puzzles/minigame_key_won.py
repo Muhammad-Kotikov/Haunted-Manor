9a3ae554a3e10421df3238ba8aa2273741854de8
Muhammad-Kotikov <muhammad.kotikov@outlook.de>
2024-12-17 22:44:43 +0100

    Removed even more files

--- src/puzzles/minigame_key_won.py
+++
@@ -1,8 +0,0 @@
-from main import * 
-
-def winning_message():
-    SCREEN.fill(BLACK)
-    winning_text = SMALL_FONT.render("Congratulations!", True, WHITE)
-    text_rect = winning_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-    SCREEN.blit(winning_text, text_rect)
-    pygame.display.flip()
